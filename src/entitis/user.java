a13e858801c6fd18e249ee515ce376117cdfb35b
ASUS <ASUS@LAPTOP-VQKL7E07>
2023-10-28 23:12:10 +0700

    model

--- src/entitis/user.java
+++ src/entitis/user.java
@@ -1,5 +1,44 @@
 package entitis;
 
 public class user {
-	private String pass;
+	private int id;
+	private String name;
+	private String address;
+	private int phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public user(int id, String name, String address, int phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+	public user() {
+		super();
+	}
+	
+	
 }
